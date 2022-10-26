882ca60bc1533db5bf3e3a40a6ccb4eb178a0df4
Andre Moraes <andre.moraes.98@gmail.com>
2022-10-26 20:39:49 -0300

    feat: conclusão da página principal

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import './Main.css';
 
 function Main() {
-  return <h1 className="text-center">First</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <section className="my-5">
+      <h2 className="text-center m-0 mx-4 py-5">
+        We are a tech provider for the International Education Industry!
+      </h2>
+
+      <p className="text-center m-0 mx-4">
+        Do you wanna get envolved?
+      </p>
+
+      <div className="main-button text-center mx-auto py-4">
+        <Button
+          variant="outline-primary"
+          onClick={() => navigate('/forms')}
+        >
+          Let&apos;s do it together!
+        </Button>
+      </div>
+    </section>
+  );
 }
 
 export default Main;
