ace0d74efa7fbc5bf0df86e5b346c3864675e76a
Andre Moraes <andre.moraes.98@gmail.com>
2022-10-26 20:40:42 -0300

    test: refatoração para inclusão do beforeEach

--- src/test/Header.test.js
+++ src/test/Header.test.js
@@ -1,22 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../components/header/Header';
 
-describe('Testa o componente Header', () => {
-  test('se a imagem foi renderizada;', () => {
+describe('Testa o componente Header:', () => {
+  beforeEach(() => {
     render(<Header />);
+  });
+
+  test('se a imagem foi renderizada;', () => {
     const imgHeader = screen.getByAltText('Ally Logo');
     expect(imgHeader).toBeInTheDocument();
   });
 
   test('se o título foi renderizado;', () => {
-    render(<Header />);
     const headerTItle = screen.getByRole('heading', { level: 1 });
     expect(headerTItle).toBeInTheDocument();
   });
 
   test('se o título está correto;', () => {
-    render(<Header />);
     const headerTItle = screen.getByRole('heading', { level: 1 });
     expect(headerTItle).toHaveTextContent("With Ally it's easier");
-  })
+  });
 });
