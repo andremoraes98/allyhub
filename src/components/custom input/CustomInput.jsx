b960e88391e2d95ffa3f3a9c7b5273ef4d79bad7
Andre Moraes <andre.moraes.98@gmail.com>
2022-10-27 09:24:59 -0300

    feat: criação do componente CustomInput

---
+++ src/components/custom input/CustomInput.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+
+function CustomInput({
+  value, handleChange, touched, errors, type, label, id,
+}) {
+  return (
+    <FloatingLabel className="my-3 mx-5" controlId={id} label={label}>
+      <Form.Control
+        type={type}
+        placeholder={label}
+        onChange={handleChange}
+        value={value[id]}
+        isValid={touched[id] && !errors[id]}
+        isInvalid={!!errors[id]}
+      />
+      <Form.Control.Feedback type="invalid">
+        {errors[id]}
+      </Form.Control.Feedback>
+
+      <Form.Control.Feedback>
+        Perfeito!
+      </Form.Control.Feedback>
+    </FloatingLabel>
+  );
+}
+
+CustomInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  touched: PropTypes.bool.isRequired,
+  errors: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+export default CustomInput;
