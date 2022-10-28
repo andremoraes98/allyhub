98484ccd2dbe589d3a69bbae2417ff66254dc855
Andre Moraes <andre.moraes.98@gmail.com>
2022-10-28 10:08:45 -0300

    feat: implementação da rota para renderizar a página Success

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Pessoal from './pages/forms/pessoal/Pessoal';
 import Location from './pages/forms/location/Location';
 import Main from './pages/main/Main';
+import Success from './pages/success/Success';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/pessoal" element={<Pessoal />} />
         <Route exact path="/location" element={<Location />} />
+        <Route exact path="/success" element={<Success />} />
         <Route exact path="/" element={<Main />} />
       </Routes>
     </>
